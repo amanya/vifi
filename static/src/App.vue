1a00c17feaad61f5810024bf2f41f780007623e4
Albert Manyà <albertmp@eml.cc>
2018-06-10 13:08:01 +0200

    Fix layout

--- static/src/App.vue
+++ static/src/App.vue
@@ -1,47 +1,48 @@
 <template>
   <div>
-    <Navbar />
-    <div class="columns is-fullheight">
-      <div v-if="isAuthenticated" class="column is-2 is-sidebar-menu is-hidden-mobile">
-        <aside v-if="loaded" class="menu">
-          <p class="menu-label">
-            General
-          </p>
-          <ul class="menu-list">
-            <li>
-              <router-link :to='{ name: "alerts" }' active-class='is-active'>
-                Alerts
-                <span v-if="unAckAlerts" class="badge is-badge-danger is-badge-medium" :data-badge="numUnAckAlerts"></span>
-              </router-link>
-            </li>
-          </ul>
-          <p class="menu-label">
-            Vineyards
-          </p>
-          <ul class="menu-list">
-            <li v-for="vineyard in vineyards" :key="vineyard.id">
-              <router-link :to='{ name: "vineyard", params: { id: vineyard.id } }' active-class='is-active'>
-                {{ vineyard.name }}
-              </router-link>
-            </li>
-          </ul>
-        </aside>
-      </div>
-      <div class="column is-main-content section">
+    <navbar />
+    <div class="columns">
+      <aside v-if="isAuthenticated" class="column menu is-2 is-sidebar-menu is-hidden-mobile aside">
+        <p class="menu-label">
+          General
+        </p>
+        <ul class="menu-list">
+          <li>
+            <router-link :to='{ name: "alerts" }' active-class='is-active'>
+              Alerts
+              <span v-if="unAckAlerts" class="badge is-badge-danger is-badge-medium" :data-badge="numUnAckAlerts"></span>
+            </router-link>
+          </li>
+        </ul>
+        <p class="menu-label">
+          Vineyards
+        </p>
+        <ul class="menu-list">
+          <li v-for="vineyard in vineyards" :key="vineyard.id">
+            <router-link :to='{ name: "vineyard", params: { id: vineyard.id } }' active-class='is-active'>
+              {{ vineyard.name }}
+            </router-link>
+          </li>
+        </ul>
+      </aside>
+      <div class="column is-main-content is-10 section">
         <router-view />
       </div>
     </div>
+    <foot />
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar'
+import Foot from '@/components/Foot'
 import { mapState, mapGetters } from 'vuex'
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    Foot
   },
   computed: {
     ...mapGetters({
@@ -74,14 +75,4 @@ $navbar-height: 3.25rem
 .is-sidebar-menu
   padding: 2.5rem
 
-.columns
-  &.is-fullheight
-    min-height: calc(100vh - ( #{$navbar-height} - .75rem ) )
-    max-height: calc(100vh - ( #{$navbar-height} - .75rem ) )
-    height: calc(100vh - ( #{$navbar-height} - .75rem ) )
-    display: flex
-    flex-direction: row
-    justify-content: stretch
-    .column
-      overflow-y: auto
 </style>
